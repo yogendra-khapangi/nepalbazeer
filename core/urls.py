512ab63757fc913d0b413c2194a17f0c511606a4
yogendra <youyogendra@gmail.com>
2024-07-05 09:44:51 +0545

    adding end points and ends points html page also

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from core.views import index
+from core.views import *
 
 app_name="core"
 
 urlpatterns = [
     path('', index),
+    path('about/',about ),
+    path('contact/', contact),
+    path('traker/', traker),
+    path('search/',search ),
+    path('productview/', productview),
+    path('checkout/', checkout),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"core/index.html")
+
+def about(request):
+    return render(request,"core/about.html")
+
+def contact(request):
+    return render(request,"core/contact.html")
+
+def traker(request):
+    return render(request,"core/traker.html")
+
+def search(request):
+    return render(request,"core/search.html")
+
+def productview(request):
+    return render(request,"core/productview.html")
+
+def checkout(request):
+    return render(request,"core/checkout.html")
+
+
